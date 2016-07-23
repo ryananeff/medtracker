6d5f96be0b20bb513f6c64bfa84dde91a3678bfc
Ryan Neff <neffra@suretify.co>
2016-07-23 18:18:31 -0400

    Built SMS survey taking code.

--- medtracker/triggers.py
+++ medtracker/triggers.py
@@ -3,7 +3,7 @@ from medtracker.models import *
 from medtracker.forms import *
 from medtracker.email_helper import send_email
 import sys
-import urllib, re
+import urllib, re, random, string
 
 def run_trigger(question, response):
 	sys.stderr.write("triggering")
@@ -11,21 +11,23 @@ def run_trigger(question, response):
 		message = question.trigger.title
 		split_message = re.split('(<.+?>)',message)
 		for ix,i in enumerate(split_message):
-			tag = re.findall('<(.+?)>',i)
+			tag = re.findall('<(.+?)>',i) # look for tags we need to replace
 			if tag != []:
 				split_tag = tag[0].split(".")
-				if len(split_tag) == 2:
+				if len(split_tag) == 2: # db type is the resource type (question, trigger, callback, etc.), while the ID is actually the position in the survey
 					db_type, db_id = split_tag
-					db_subtype = None
+					db_subtype = None # subtype is if the element is multi-part (e.g. questions have elements attached)
 				if len(split_tag) == 3:
 					db_type, db_id, db_subtype = split_tag
 				if db_type.lower() == 'question':
 					question_ids = [q.id for q in response.question.survey.questions]
 					result = QuestionResponse.query.filter_by(question_id=question_ids[int(db_id)-1], uniq_id=response.uniq_id).first() if question_ids != [] else None
 					if result != None:
-						split_message[ix] = result.response
+						split_message[ix] = result.response # this is the default subtype (response)
+							#TODO: this needs to be fixed for things like yes or no questions or 1 to 10
+							#        * maybe in the models??
 					else:
-						split_message[ix] = ""
+						split_message[ix] = "" # when there's nothing there
 		message = "".join(split_message)
 		if question.trigger.recipients == None:
 			print "No valid recipients"
@@ -33,6 +35,7 @@ def run_trigger(question, response):
 		recipients = question.trigger.recipients
 		callback = None
 		if question.trigger.kind == 'voice':
+			# TODO: needs to check if the recipient type is actually valid or else internal server error!
 			phone_trigger(message, recipients, callback)
 		elif question.trigger.kind == 'sms':
 			sms_trigger(message, recipients, callback)
@@ -75,6 +78,115 @@ def sms_trigger(message, recipients, callback):
 		)
 	return None
 
+@app.route("/test_sms/<int:survey_id>/<phone_number>", methods=["GET", "POST"])
+def test_sms_survey(survey_id, phone_number):
+	uniq_id = randomword(64)
+	survey = Survey.query.get_or_404(survey_id)
+	task = Progress(
+		user = phone_number, 
+		task = survey.id,
+		parent_id = uniq_id)
+	db_session.add(task)
+	db_session.commit()
+	msg = serve_sms_survey(task)
+	hello = "Hello! You have a new incoming test survey from Suretify!"
+	sms_trigger(hello, phone_number, None)
+	sms_trigger(msg, phone_number, None)
+	return "Task successfully queued."
+
+
+@app.route("/sms", methods=["GET", "POST"])
+def sms_gather_information():
+	# TODO: this function needs to connect to a database that has a list of phone numbers, the current task that they are on, the iterator, and the initiator ID.
+	# this will then allow us to handle survey answers and ask the next question
+	from_phone = request.values.get("From", "+10000000000")
+	from_phone = from_phone[-10:]
+	message = request.values.get("Body", None)
+	current_tasks = Progress.query.filter_by(user=from_phone, complete=0).all()
+	resp = twilio.twiml.Response()
+	body = message.strip()
+	if len(current_tasks) == 0:
+		resp.message("You're already done responding!")
+	else:
+		last_task = current_tasks[0]
+		# now we need to save the response if correct
+		next_task, error = save_sms_survey(last_task, body)
+		if error:
+			resp.message(error)
+		if next_task:
+			msg = serve_sms_survey(next_task)
+			resp.message(msg)
+	return str(resp)
+
+def serve_sms_survey(task):
+	# TODO: this function will allow us to specify a survey as one of the callback functions and to ask it. Think a template is overkill here though.
+	question, question_id, next_question, last_question = get_current_question(task)
+	qlabel = [b for a,b in QUESTION_KIND_CHOICES if a==question.kind][0]
+	return question.body + " (" + qlabel.lower() + ")"
+
+def save_sms_survey(task, body):
+	# TODO: this function will allow us to specify a survey as one of the callback functions and to ask it. Think a template is overkill here though.
+	question, question_id, next_question, last_question = get_current_question(task)
+	if next_question == None:
+		_ = increment_iterator(task)
+		return (None, "Thank you for your responses. You're done!")
+	if question.kind != "text":
+		body = body.lower()
+		if question.kind == "numeric":
+			if body not in [str(i) for i in range(1,10)]:
+				return (task, "Please choose a number from 1 to 10.")
+		if question.kind == "yes-no":
+			if body not in ["yes", "y", "no", "n", "yes (y)", "no (n)"]:
+				return (task, "Please respond with only YES (y) or NO (n)")
+	save_basic_response(body, task.parent_id, question_id)
+	new_task = increment_iterator(task)
+	return (new_task, None)
+
+def save_basic_response(message, uniq_id, question_id):
+	_response = QuestionResponse(
+		message,
+		uniq_id,
+		None,
+		question_id
+	)
+	_response.user_id = None
+	db_session.add(_response)
+	db_session.commit()
+	question = _response._question
+	if _response.uniq_id != None:
+		sys.stderr.write('starting trigger')
+		run_trigger(question, _response)
+	return True
+
+def get_current_question(task):
+	survey = Survey.query.get_or_404(int(task.task))
+	question_id = task.iterator
+	question_ids = [q.id for q in survey.questions]
+	if len(question_ids) == 0:
+			return None, None, None, None
+	if (question_id == None) | (question_id == ''): # if on first question
+		question_id = question_ids[0]
+	question_id = int(question_id)
+	curpos = question_ids[question_id]
+	question = Question.query.get_or_404(curpos)
+	next_question = question_id+1 if curpos+1 < len(question_ids) else None
+	last_question = question_id-1 if curpos-1 >= 0 else None
+	return question, curpos, next_question, last_question
+
+def increment_iterator(task):
+	question, question_id, next_question, last_question = get_current_question(task)
+	if next_question == None:
+		task.complete = 1
+	task.iterator = next_question
+	db_session.add(task)
+	db_session.commit()
+	return task
+
+def serve_voice_survey(survey_id, iterator, uniq_id):
+	#TODO: serve surveys from trigger callbacks over the phone and save them to DB
+
+	return None
+
 def email_trigger(message, recipients, callback):
 	for r in recipients.split(";"):
 		subject = "Request for information from Suretify"
@@ -89,3 +201,7 @@ def url_trigger(message, recipients, callback):
 	for r in recipients.split(";"):
 		requests.get(r % (urllib.quote(message)))
 	return None
+
+def randomword(length):
+	'''generate a random string of whatever length, good for filenames'''
+	return ''.join(random.choice(string.lowercase) for i in range(length))
